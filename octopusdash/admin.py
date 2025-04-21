6d1a567cc999f8bc29c73184379e0f6911e0f243
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-04-21 23:22:07 +0300

    installing django_related_objects_fetcher

--- octopusdash/admin.py
+++ octopusdash/admin.py
@@ -1,24 +1,3 @@
 from django.contrib import admin
 from .registry import dashboard,ModelAdmin
 
-
-def mark_as_published(modeladmin, request, queryset):
-    updated = queryset
-    modeladmin.message_user(request, f"{updated} post(s) marked as published.")
-
-mark_as_published.short_description = "Mark selected posts as published"
-# Register your models here.
-from .models import Post,Book
-
-class PostAdminModel(ModelAdmin):
-    list_display = ['id','title','content','duration']
-
-dashboard.register(Post,PostAdminModel)
-dashboard.register(Book)
-
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','content']
-    actions = [mark_as_published]
