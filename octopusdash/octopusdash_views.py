3f056100213a11fdbb5fddd90f679fa4c15274cc
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-04-24 17:30:51 +0300

    Moving octopusdash views to another file and adding UserListView,UpdateUserView

---
+++ octopusdash/octopusdash_views.py
@@ -0,0 +1,73 @@
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.generic import ListView,UpdateView
+from django.contrib.auth import get_user_model
+from .registry import ModelAdmin
+from .views_mixin import SearchFilterMixin
+from .forms import UserModelForm
+User = get_user_model()
+
+
+class UpdateUserView(UpdateView):
+    model = User
+    form_class = UserModelForm
+    template_name = 'users/update_user.html'
+    
+    def get_success_url(self):
+        return self.request.path
+
+    def form_valid(self, form):
+        instance = form.save()
+        messages.success(self.request, f"Successfully updated {self.model._meta.verbose_name}: {instance}.")
+        return super().form_valid(form)
+
+class UserAdmin(ModelAdmin):
+    list_display = ['username','is_superuser','is_staff','first_name','last_name','email','is_active']
+
+class UserListView(SearchFilterMixin,ListView):
+    model = User
+    queryset = User.objects.all()
+    model_admin = UserAdmin(User)
+    model_name = User._meta.model_name
+    app_name = User._meta.app_label
+    paginate_by = 10
+    context_object_name = 'users'
+    template_name = 'users/users_list.html'
+    
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['model_name'] = self.model_name
+        context['app_name'] = self.app_name
+        context['model_admin'] = self.model_admin
+        
+        return context
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            if user.is_staff:
+                login(request, user)
+                messages.success(request, "Logged in successfully.")
+                return redirect("octopusdash-dashboard")  # update route as needed
+            else:
+                messages.error(request, "You are not authorized to access the admin dashboard.")
+                # optional: logout just in case
+                logout(request)
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "authentication/login.html")
+
+
+def dashboard_view(request):
+
+    
+    return render(request,'index.html')
