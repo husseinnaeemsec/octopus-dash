f97553f2f3326693a9db1bc8cf8fa6395281a10d
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-04-24 17:33:28 +0300

    setting up a default settings

--- octopusdash/conf.py
+++ octopusdash/conf.py
@@ -1,10 +1,19 @@
 from django.conf import settings as django_settings
 
+default_settings = {
+        "INCLUDE_ALL_APPS":False,
+        'USER_FORM_FIELDS':[
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'user_permissions',
+            'groups',
+        ]
+    }
+
 try:
     settings = django_settings.OCTOPUSDASH_SETTINGS
-
 except AttributeError:
-    
-    settings = {
-        "INCLUDE_ALL_APPS":False
-    }
+    settings = default_settings
