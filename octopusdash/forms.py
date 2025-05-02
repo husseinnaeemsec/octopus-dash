3ecee2e4d990235280f044de118b811866a9b9e1
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-05-02 22:13:16 +0300

    Clean up

--- octopusdash/forms.py
+++ octopusdash/forms.py
@@ -12,16 +12,18 @@ User = get_user_model()
 
 
 
-
-class DynamiModelForm(forms.ModelForm):
+class DynamicModelForm(forms.ModelForm):
     
     fields_metadata = {}
     utils = importlib.import_module
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)  # Corrected super call
-        widget = None
-        # Optionally, you can loop through fields and print them or customize further
+
+        # Loop through the form fields to dynamically set widgets
         for field_name, field in self.fields.items():
+            
+            widget = None
             # Handle SlugField
             if isinstance(field.widget, forms.TextInput) and field.__class__.__name__ == 'SlugField':
                 widget = fields.ODSlugInput(field=field)
@@ -31,15 +33,15 @@ class DynamiModelForm(forms.ModelForm):
                 widget = fields.ODTimeInput(field=field)
 
             # Handle DateInput
-            elif isinstance(field.widget, forms.DateInput) and field.__class__.__name__ == 'DateField' :
+            elif isinstance(field.widget, forms.DateInput) and field.__class__.__name__ == 'DateField':
                 widget = fields.ODDateInput(field=field)
 
             # Handle DateTimeInput
             elif isinstance(field.widget, forms.DateTimeInput) and field.__class__.__name__ == 'DateTimeField':
-                print(field_name, 'Is Datetime input')
                 widget = fields.ODDateTimeInput(field=field)
 
-            elif isinstance(field.widget,forms.FileInput):
+            # Handle FileInput
+            elif isinstance(field.widget, forms.FileInput):
                 widget = fields.ODFileInput(field=field)
 
             # Handle other TextInput fields (default)
@@ -60,17 +62,17 @@ class DynamiModelForm(forms.ModelForm):
 
             # Handle Textarea
             elif isinstance(field.widget, forms.Textarea):
-                widget = fields.ODTextArea()
+                widget = fields.ODTextArea(field=field)
 
             # Handle SelectMultiple
             elif isinstance(field.widget, forms.SelectMultiple):
                 choices = getattr(field, 'choices', [])
-                widget = fields.ODSelectMultiple(choices=choices)
+                widget = fields.ODSelectMultiple(choices=choices,field=field)
 
             # Handle Select
             elif isinstance(field.widget, forms.Select):
                 choices = getattr(field, 'choices', [])
-                widget = fields.ODSelect(choices=choices)
+                widget = fields.ODSelect(choices=choices,field=field)
 
             # Handle CheckboxInput
             elif isinstance(field.widget, forms.CheckboxInput):
@@ -78,21 +80,33 @@ class DynamiModelForm(forms.ModelForm):
 
             # Handle URLInput
             elif isinstance(field.widget, forms.URLInput):
-                widget = fields.ODURLField()
+                widget = fields.ODURLField(field=field)
 
-            # If no match, continue
-            else:
-                continue
-
+            # If no match, continue to the next field
             if widget is not None:
                 field.widget = widget
+                
+
     class Meta:
         model = None
         fields = "__all__"
 
-    
     def get_media(self):
+        # Ensure that any media (JS, CSS) related to the widgets is included
         return self.media
+
+    def clean(self):
+        cleaned_data = super().clean()
+        self._files_to_remove = [] 
+        for field_name, field in self.fields.items():
+            if isinstance(field, (forms.FileField,forms.ImageField)):
+                remove_flag = f"{field_name}_removefile"
+                print(self.data)
+                if self.data.get(remove_flag) == '1':
+                    cleaned_data[field_name] = None
+                    self._files_to_remove.append(field_name)
+        return cleaned_data
+
 
 
 form_class_fields = settings.get("USER_FORM_FIELDS",default_settings.get("USER_FORM_FIELDS"))
@@ -100,7 +114,7 @@ exclude_fields = []
 
 if 'password' in form_class_fields:
     exclude_fields = ['password']
-class UserModelForm(DynamiModelForm):
+class UserModelForm(DynamicModelForm):
     
     class Meta:
         model = User
@@ -117,7 +131,7 @@ def form_factory(model, fields='__all__'):
         fields = cls_fields
     
     # Dynamically create a form class with the dynamic Meta class
-    form_class = type(f"DynamicModelForm", (DynamiModelForm,), {"Meta": Meta})
+    form_class = type(f"DynamicModelForm", (DynamicModelForm,), {"Meta": Meta})
     
     return form_class
 
