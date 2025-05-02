02c7664be2688686c72e3b0df4c9eb4360f24276
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-05-02 13:03:33 +0300

    "Testing" a test view to render the images from Trix content

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import SimpleBook,ContentImage
 # Create your views here.
+
+def display_book(request):
+    book = SimpleBook.objects.last()
+    
+    print("Called")
+    
+    return render(request,'index.html',{'book':book})
