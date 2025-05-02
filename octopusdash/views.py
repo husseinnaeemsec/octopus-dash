e895d46396e296e5cad9c4a2c03a890bb3202f06
Hussein Naeem <husseinnaeemsec@gmail.com>
2025-05-02 13:12:10 +0300

    Changing template structure

--- octopusdash/views.py
+++ octopusdash/views.py
@@ -11,6 +11,12 @@ from django.db.models import ProtectedError,RestrictedError
 from .django_relatedobjects_fetcher.related_objects_fetcher import RelatedObjectsCollector
 from django.db import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
+from .conf import settings
+from django.apps import apps
+from .forms import form_factory
+from .utils import get_image_creation_model
+from bs4 import BeautifulSoup
+
 
 from .registry import dashboard
 # Create your views here.
@@ -19,7 +25,7 @@ registry = dashboard.get_registry()
 
 class ModelListView(ListViewMixin,ListView):
     model = None
-    template_name = 'dynamic/list.html'
+    template_name = 'octopusdash/dynamic/list.html'
     paginate_by  = 10
     context_object_name = 'objects'
 
@@ -48,12 +54,12 @@ class ModelListView(ListViewMixin,ListView):
 
 
 class CreateInstanceView(ModelContextMixin,CreateView):
-    template_name = 'dynamic/create.html'
+    template_name = 'octopusdash/dynamic/create.html'
 
     def get_success_url(self):
         success_action = self.request.POST.get("__success__",None)
         
-        if success_action == 'save-add':
+        if success_action == 'save-add' and self.request.method == 'POST':
             self.success_url = reverse_lazy('create-object',args=[self.app_name,self.model_name])
 
         return self.success_url
@@ -64,17 +70,60 @@ class CreateInstanceView(ModelContextMixin,CreateView):
 
 
     def form_valid(self, form):
-        # Save the new instance
-        instance = form.save()
-        # Add a success message with the instance's data
-        messages.success(self.request, f"Successfully created {self.model._meta.verbose_name}: {instance}.")
+        # Save the object first
+        response = super().form_valid(form)
+        instance = self.object
+        print(self.request.FILES)
+        # If this model has image creation enabled
+        # Now: process each Trix field to extract image references
+        for field in instance._meta.fields:
+            if self.model_admin.fields_config.get(field.name):
+                field_config = self.model_admin.fields_config.get(field.name)
+                app_label, module, model_name = field_config.get("model").split(".")
 
-        # Redirect to another page (could be the list view or detail view of the instance)
-        return HttpResponseRedirect(self.success_url)
+                try:
+                    image_creation_model = apps.get_model(app_label, model_name)
+                    image_creation_form = form_factory(image_creation_model)
+                    file_field_name = field_config.get("file_field_name")
+                    instance_name = field_config.get("model_field_name")
 
+                    images = self.request.FILES.getlist(f"{field.name}_image")
+                    uploaded_image_urls = []
+
+                    for image in images:
+                        data = {
+                            instance_name: instance
+                        }
+                        files = {
+                            file_field_name: image
+                        }
+                        form = image_creation_form(data=data, files=files)
+
+                        if form.is_valid():
+                            image_instance = form.save()
+                            uploaded_image_urls.append(image_instance.image.url)  # or getattr(image_instance, file_field_name).url
+                        else:
+                            print("Image form errors:", form.errors)
+
+                    # Replace placeholder <img src=""> in the original content
+                    content = getattr(instance, field.name)
+                    soup = BeautifulSoup(content, "html.parser")
+
+                    for img_tag, real_url in zip(soup.find_all("img"), uploaded_image_urls):
+                        img_tag["src"] = real_url
+
+                    # Save updated content back to instance
+                    setattr(instance, field.name, str(soup))
+                    instance.save(update_fields=[field.name])
+                except LookupError:
+                    raise LookupError(f"Model with the name {field_config.get("model")} can not be found to assosiate with {field.name} ")
+                except Exception as e:
+                    raise Exception(f"Unknown error  {e} ")
+                
+        return response
 
 class UpdateInstanceView(ModelContextMixin,UpdateView):
-    template_name = 'dynamic/update.html'
+    template_name = 'octopusdash/dynamic/update.html'
     context_object_name = 'object'
 
     
@@ -100,8 +149,8 @@ class UpdateInstanceView(ModelContextMixin,UpdateView):
 
 
 class DeleteInstanceView(ModelContextMixin,DeleteView):
-    template_name = 'dynamic/delete.html'
-    error_template = 'errors/object_not_found.html'
+    template_name = 'octopusdash/dynamic/delete.html'
+    error_template = 'octopusdash/errors/object_not_found.html'
     context_object_name = 'object'
     
     def get(self, request, *args, **kwargs):
@@ -149,7 +198,7 @@ class AppView(AppLookupMixin,View):
     
     
     def get(self,request,app,*args,**kwargs):
-        return render(request,'dynamic/app.html')
+        return render(request,'octopusdash/dynamic/app.html')
 
 
 
@@ -157,7 +206,7 @@ class ModelView(ModelLookupMixin,View):
     
     
     def get(self,request,app,*args,**kwargs):
-        return render(request,'dynamic/model.html')
+        return render(request,'octopusdash/dynamic/model.html')
 
 
 
@@ -170,4 +219,4 @@ def apps_view(request):
     
     
     
-    return render(request,'apps_list.html')
+    return render(request,'octopusdash/apps_list.html')
